c3dedd94b27fd238c657c664fdfa26e5c9d19efe
Baekhyungi <vhxm8155@naver.com>
2024-06-27 16:05:31 +0900

    .

--- app/src/main/java/bitcamp/project1/Test1.java
+++ app/src/main/java/bitcamp/project1/Test1.java
@@ -1,4 +1,11 @@
 package bitcamp.project1;
 
 public class Test1 {
+    void test1() {
+        System.out.println("git Test 입니다.");
+    }
+
+    static void m3() {
+        System.out.println("오호라!!!");
+    }
 }
